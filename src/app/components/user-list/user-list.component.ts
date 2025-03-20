c38fed7a85f271a04d5792911ef139e1809af42c
Tirta Saraswati <tirta.saraswati@majoo.id>
2025-03-20 11:32:39 +0700

    Task 2 - add loading state and handling error

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
+import { Observable, catchError, of, tap } from 'rxjs';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -9,13 +11,21 @@ import { UserService } from '../../services/user.service';
   styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
-  users: any[] = [];
+  users$!: Observable<User[]>;
+  errorMessage = '';
+  loading = true;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.getListUsers().subscribe((data) => {
-      this.users = data;
-    });
+    this.users$ = this.userService.getListUsers().pipe(
+      tap(() => (this.loading = false)),
+      catchError((error) => {
+        this.loading = false;
+        this.errorMessage =
+          error.message || 'Failed to get data User. Please try again later!';
+        return of([]);
+      })
+    );
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class UserService {
   private apiUser = 'https://jsonplaceholder.typicode.com/users';
 
   constructor(private http: HttpClient) {}
 
   getListUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUser);
+    return this.http.get<User[]>(this.apiUser).pipe(
+      catchError((error) => {
+        console.error('Error fetch data users:', error);
+        return throwError(
+          () => new Error('Failed to get data User. Please try again later!')
+        );
+      })
+    );
   }
 }
