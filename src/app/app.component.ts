5e3de9653fc62955d9c550107e633a17cfb05b3b
Tirta Saraswati <tirta.saraswati@majoo.id>
2025-03-20 09:44:29 +0700

    Task 2 - display table and add dummy data users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
-
+  users = [
+    { name: 'Tirta', email: 'alice@example.com', website: 'tirta.com' },
+    { name: 'Lisa', email: 'bob@example.com', website: 'lisa.com' }
+  ];
 }
